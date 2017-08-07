ee55da25c9e8ac96fec2e7e17cb71e69b12fa58e
Austin DeGarmo <austindegarmo@yahoo.com>
2017-08-06 21:56:41 -0700

    pusher

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE eat_db;
+USE eat_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	date int, 
+	PRIMARY KEY (id)
+);
